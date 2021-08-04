cb521e9f8d81a09bfa22e247a32aa38f5c80d6ea
Bevan Kay <email@bevankay.me>
2021-08-04 15:30:40 +1000

    fix fluro-printer style

--- Casks/b-fluro-printer.rb
+++ Casks/b-fluro-printer.rb
@@ -2,8 +2,9 @@ cask "b-fluro-printer" do
   version "6.2.4"
   sha256 "86a0587db197a0995524e0d456117f037d77e2404db470590bc221caa917b4b8"
 
-  url "https://api.fluro.io/download/58afc74c7b3e1d3b73f7a949"
+  url "https://api.fluro.io/download/58afc74c7b3e1d3b73f7a949?version=#{version}"
   name "b-fluro-printer"
+  desc "Fluro printer app"
   homepage "https://support.fluro.io/view/checkin-printer"
 
   livecheck do

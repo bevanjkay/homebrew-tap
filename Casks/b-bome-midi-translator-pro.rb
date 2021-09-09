a5107a43858dc8defa2fcc16b8cef5910ca1e976
Bevan Kay <email@bevankay.me>
2021-09-09 13:55:37 +1000

    add bome midi translator 1.9.0

---
+++ Casks/b-bome-midi-translator-pro.rb
@@ -0,0 +1,21 @@
+cask "b-bome-midi-translator-pro" do
+  version "1.9.0"
+  sha256 "37436933ce487fbd4054277d94863ad3d244728263535cc61d900feba59440a6"
+
+  url "https://download.bome.com/dl.php/16D8988A37FC7/MIDITranslatorPro#{version}_Full.dmg"
+  name "b-bome-midi-translator-pro"
+  desc "MIDI Translator Application"
+  homepage "https://www.bome.com/products/miditranslator"
+
+  livecheck do
+    url "https://www.bome.com/products/miditranslator/support/updates?v=#{version}&e=pro"
+    regex(/The current version of MIDI Translator Pro is (\d+(?:\.\d+)+)/i)
+  end
+
+  app "Bome MIDI Translator Pro.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.bome.miditranslator.pro.plist",
+    "~/Library/Saved Application State/com.bome.miditranslator.pro.savedState",
+  ]
+end

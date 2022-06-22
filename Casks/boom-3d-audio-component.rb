4bdd3575d9ba4f7a554b30e9e54cb46edb30d24e
Bevan Kay <email@bevankay.me>
2022-06-22 11:01:15 +1000

    Add boom-3d-audio-component

--- Casks/boom-3d-audio-component.rb
+++ Casks/boom-3d-audio-component.rb
@@ -1,8 +1,9 @@
 cask "boom-3d-audio-component" do
   version "1.0"
-  sha256 "ab05b0e94472f2f1d373adedd5e3a72a5552a5083af0377cfe68b4edc8ea2fcc"
+  sha256 :no_check
 
-  url "https://d3jbf8nvvpx3fh.cloudfront.net/device-assets/boom3d/catalina/Boom3d_Audio_Component_Installer.zip"
+  url "https://d3jbf8nvvpx3fh.cloudfront.net/device-assets/boom3d/catalina/Boom3d_Audio_Component_Installer.zip",
+      verified: "d3jbf8nvvpx3fh.cloudfront.net/device-assets/boom3d/"
   name "Boom 3D Component Installer"
   desc "Component installer for Boom 3D"
   homepage "https://www.globaldelight.com/boom3d"
@@ -13,11 +14,12 @@ cask "boom-3d-audio-component" do
   end
 
   app "Audio Component Installer.app"
-
   installer manual: "Audio Component Installer.app"
 
   postflight do
     system "open", "/Applications/Audio Component Installer.app"
-    system "rm", "/Applications/Audio Component Installer.app"
+    system "rm -r", "/Applications/Audio Component Installer.app"
   end
+
+  uninstall trash: "/Applications/Audio Component Installer.app"
 end

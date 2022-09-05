e765699782a9a6f9fbca27cb97fdd095f00c56ec
Bevan Kay <email@bevankay.me>
2022-09-05 10:29:54 +1000

    update style

--- Casks/b-hyper.rb
+++ Casks/b-hyper.rb
@@ -1,15 +1,12 @@
 cask "b-hyper" do
-  version "3.4.0-canary.1"
+  arch arm: "arm64", intel: "x64"
 
-  if Hardware::CPU.intel?
-    sha256 "e1ca5dd3685613bc944c03285611ac2739c450800ed35048edaa9f69458ddfeb"
-    url "https://github.com/vercel/hyper/releases/download/v#{version}/Hyper-#{version}-mac-x64.zip",
-        verified: "github.com/vercel/hyper/"
-  else
-    sha256 "66b3550de27ec0afde359e6fd5105176bd6446a7cfc11964636156bcbfb8724f"
-    url "https://github.com/vercel/hyper/releases/download/v#{version}/Hyper-#{version}-mac-arm64.zip",
-        verified: "github.com/vercel/hyper/"
-  end
+  version "3.4.0-canary.1"
+  sha256 arm:   "66b3550de27ec0afde359e6fd5105176bd6446a7cfc11964636156bcbfb8724f",
+         intel: "e1ca5dd3685613bc944c03285611ac2739c450800ed35048edaa9f69458ddfeb"
+
+  url "https://github.com/vercel/hyper/releases/download/v#{version}/Hyper-#{version}-mac-#{arch}.zip",
+      verified: "github.com/vercel/hyper/"
   name "Hyper"
   desc "Terminal built on web technologies"
   homepage "https://hyper.is/"

50c5c00bb5c95e628fce71b9c7eab93ae03be693
Bevan Kay <email@bevankay.me>
2023-03-20 15:17:13 +1100

    Add Mixing Station

---
+++ Casks/mixing-station.rb
@@ -0,0 +1,24 @@
+cask "mixing-station" do
+  version "1.8.6"
+  sha256 :no_check
+
+  url "https://mixingstation.app/backend/api/web/download/attachment/mixing-station-pc/release/OSX"
+  name "Mixing Station"
+  desc "Audio Mixer Controller"
+  homepage "https://mixingstation.app/"
+
+  livecheck do
+    url "https://mixingstation.app/backend/api/web/changelogs/milestones"
+    strategy :json do |json|
+      json["data"].select { |item| item["variant"] == "PC" }
+                  .map { |item| item["current"]["name"] }
+    end
+  end
+
+  app "mixing-station-pc.app", target: "Mixing Station.app"
+
+  zap trash: [
+    "~/Library/Saved Application State/org.devcore.mixingstation.pc.savedState",
+    "~/MixingStation",
+  ]
+end

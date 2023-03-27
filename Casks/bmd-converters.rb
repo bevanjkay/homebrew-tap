7b6c49077ba03e6c5bd2580b016d97a0cd26c408
Bevan Kay <email@bevankay.me>
2023-03-27 14:25:04 +1100

    Update bmd-converters from 8.0.0,d4c06d57c74141348a428af7d0b8b074,1f130439a7ad432abb4981ab0862dd51 to 8.0.0,d4c06d57c74141348a428af7d0b8b074,1f130439a7ad432abb4981ab0862dd51 (#78)

---
+++ Casks/bmd-converters.rb
@@ -0,0 +1,53 @@
+cask "bmd-converters" do
+  require "net/http"
+
+  version "8.0.0,d4c06d57c74141348a428af7d0b8b074,1f130439a7ad432abb4981ab0862dd51"
+  sha256 "94ffd2c180af6584e3f330680b52703778e4f6b30f94585fcd9335f4b2658128"
+
+  url do
+    params = {
+      "platform"     => "Mac OS X",
+      "downloadOnly" => "true",
+      "country"      => "us",
+      "policy"       => "true",
+    }.to_json
+
+    uri = URI("https://www.blackmagicdesign.com/api/register/us/download/#{version.csv.third}")
+    resp = Net::HTTP.post(uri, params, { "Content-Type" => "application/json" })
+
+    resp.body
+  end
+  name "Blackmagic Converters"
+  desc "Utility to update and control Blackmagic Design Converters"
+  homepage "https://www.blackmagicdesign.com/"
+
+  livecheck do
+    url "https://www.blackmagicdesign.com/"
+    strategy :page_match do
+      res, _err, _st =
+        Open3.capture3("curl -X POST -H \"Content-Type: application/json\" -d '{\"product\":\"converters\", " \
+                       "\"platform\":\"mac\"}' \"https://www.blackmagicdesign.com/api/support/latest-version\"")
+      version_info = JSON.parse(res)["mac"]
+      next if version_info.blank?
+
+      "#{version_info["major"]}.#{version_info["minor"]}.#{version_info["releaseNum"]},#{version_info["releaseId"]},#{version_info["downloadId"]}"
+    end
+  end
+
+  container nested: "Blackmagic_Converters_#{version.csv.first.chomp(".0")}.dmg"
+
+  pkg "Install Converters #{version.csv.first.chomp(".0")}.pkg"
+
+  uninstall pkgutil: [
+    "com.blackmagic-design.Converters",
+    "com.blackmagic-design.ConvertersAssets",
+    "com.blackmagic-design.ConvertersUninstaller",
+    "com.blackmagic-design.MicConverter",
+    "com.blackmagic-design.MicroConverters",
+  ]
+
+  zap trash: [
+    "~/Library/Preferences/com.blackmagic-design.Converters Setup.plist",
+    "~/Library/Saved Application State/com.blackmagic-design.converters.utility.savedState",
+  ]
+end

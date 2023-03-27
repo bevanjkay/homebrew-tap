8dae3bcb6bf426c67b931045508c85d691d1c5e2
Bevan Kay <email@bevankay.me>
2023-03-27 15:32:57 +1100

    decimator-ucp: add cask (#83)
    * decimator-ucp: add
    
    * decimator-ucp: add livecheck

---
+++ Casks/decimator-ucp.rb
@@ -0,0 +1,18 @@
+cask "decimator-ucp" do
+  version "3.0.0"
+  sha256 "4b8a2cc909f89c734dcfea36dd8610cabc2d317f52c56c6a051e15974bbe93c3"
+
+  url "http://decimator.com/specs/UCP%20#{version}.dmg"
+  name "Decimator USB Control Panel"
+  desc "Utility to control and update Decimator Converters"
+  homepage "http://decimator.com/"
+
+  livecheck do
+    url "http://decimator.com/DOWNLOADS/DOWNLOADS.html"
+    regex(/href=.*?UCP[ ._-]v?(\d+(?:\.\d+)+)\.dmg/i)
+  end
+
+  app "UCP #{version}.app", target: "Decimator UCP.app"
+
+  # No zap stanza required
+end

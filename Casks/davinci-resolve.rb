6778f1a0b5d3c8de31af52d8a8285e2a57cf82fd
Bevan Kay <email@bevankay.me>
2023-03-27 13:53:30 +1100

    Update davinci-resolve from 18.1.1 to 18.1.4

---
+++ Casks/davinci-resolve.rb
@@ -0,0 +1,69 @@
+cask "davinci-resolve" do
+  require "net/http"
+
+  version "18.1.4,9,54c6aec905724e6d9a1ed087505a3dc2"
+  sha256 "7e8160409cb3d887997edad631d6dc57340e958506e48b4b365ad0189a3697f0"
+
+  url do
+    unless File.exist?("#{Dir.home}/.personal_details.json")
+      ohai "Please create a personal details file at ~/.personal_details.json - using placeholder data"
+    end
+
+    personal_details = JSON.parse(File.read("#{Dir.home}/.personal_details.json"))
+
+    params = {
+      "platform"  => "Mac OS X",
+      "product"   => "DaVinci Resolve",
+      "firstname" => personal_details["firstname"] || "Joe",
+      "lastname"  => personal_details["lastname"] || "Bloggs",
+      "email"     => personal_details["email"] || "testemail@gmail.com",
+      "phone"     => personal_details["phone"] || "61412345678",
+      "city"      => personal_details["city"] || "Melbourne",
+      "state"     => personal_details["state"] || "Victoria",
+      "country"   => personal_details["countrycode"] || "au",
+      "policy"    => "true",
+    }.to_json
+
+    uri = URI("https://www.blackmagicdesign.com/api/register/au/download/#{version.csv.third}")
+    resp = Net::HTTP.post(uri, params, { "Content-Type" => "application/json" })
+
+    resp.body
+  end
+  name "Davinci Resolve"
+  desc "Video Editing Software"
+  homepage "https://www.blackmagicdesign.com/au/products/davinciresolve/"
+
+  livecheck do
+    url "https://www.blackmagicdesign.com/"
+    strategy :page_match do
+      res, = Open3.capture3(
+        "curl -X POST -H \"Content-Type: application/json\" -d '{\"product\": \"davinci-resolve\", " \
+        "\"platform\":\"mac\"}' \"https://www.blackmagicdesign.com/api/support/latest-version\"",
+      )
+      version_info = JSON.parse(res)["mac"]
+      "#{version_info["major"]}.#{version_info["minor"]}.#{version_info["releaseNum"]},#{version_info["build"]},#{version_info["downloadId"]}"
+    end
+  end
+
+  pkg "Install Resolve #{version.csv.first}.pkg"
+
+  uninstall script:  {
+              executable: "/Applications/DaVinci Resolve/Uninstall Resolve.app/Contents/Resources/uninstall.sh",
+              sudo:       true,
+            },
+            pkgutil: [
+              "com.blackmagic-design.BlackmagicRaw_resolve",
+              "com.blackmagic-design.DaVinciKeyboards",
+              "com.blackmagic-design.DaVinciPanels",
+              "com.blackmagic-design.FairlightPanels",
+              "com.blackmagic-design.ManifestBlackmagicRawPlayer",
+              "com.blackmagic-design.ManifestPanels",
+              "com.blackmagic-design.ManifestLite",
+            ]
+
+  zap trash: [
+    "~/Library/Application Scripts/com.blackmagic-design.DaVinciResolveLite",
+    "~/Library/Containers/com.blackmagic-design.DaVinciResolveLite",
+    "~/Library/Saved Application State/com.blackmagic-design.DaVinciResolveLite.savedState",
+  ]
+end

2e4168c49fe556ca5227ad96a08f640044ef596f
Bevan Kay <email@bevankay.me>
2023-04-24 17:17:20 +1000

    dante-via: add verified url

--- Casks/dante-via.rb
+++ Casks/dante-via.rb
@@ -2,7 +2,8 @@ cask "dante-via" do
   version "1.3.1.3"
   sha256 "9fee1a9241a9a3a502e41b0f2f89a3046ed009a3831a63eb91efa1144d2a434c"
 
-  url "https://audinate-software-updates.sgp1.cdn.digitaloceanspaces.com/DanteVia/#{version.major}/#{version.major_minor}/DanteVia-#{version}_macos.dmg"
+  url "https://audinate-software-updates.sgp1.cdn.digitaloceanspaces.com/DanteVia/#{version.major}/#{version.major_minor}/DanteVia-#{version}_macos.dmg",
+      verified: "audinate-software-updates.sgp1.cdn.digitaloceanspaces.com/DanteVia/"
   name "Dante Via"
   desc "Connect application to Dante network"
   homepage "https://www.audinate.com/products/software/dante-via"

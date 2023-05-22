2a1bcfd87721ac43fe7ef357a8d1a56a423cb8b9
Bevan Kay <email@bevankay.me>
2023-05-22 14:15:07 +1000

    bmd-cloud-store: add cask (#153)

---
+++ Casks/bmd-cloud-store.rb
@@ -0,0 +1,59 @@
+cask "bmd-cloud-store" do
+  require "net/http"
+
+  version "1.1.3,95d4cc97b76c4ffcb7c446ca32995fe8,638c16039a8040ad84ca46080ff60fe3"
+  sha256 "bd78d59c5b4fa0fcebf2ef9cc9d9b271d8c8ed892554b92bb6b202f9d7f9d333"
+
+  url do
+    params = {
+      "platform"     => "Mac OS X",
+      "downloadOnly" => "true",
+      "country"      => "us",
+      "policy"       => "true",
+    }.to_json
+
+    uri = URI("https://www.blackmagicdesign.com/api/register/us/download/#{version.csv.third}")
+    resp = Net::HTTP.post(uri, params, { "Content-Type" => "application/json" })
+
+    resp.body
+  end
+  name "Blackmagic Cloud Store"
+  desc "Update and manage Blackmagic Cloud Hardware"
+  homepage "https://www.blackmagicdesign.com/products/blackmagiccloudpod"
+
+  livecheck do
+    url "https://www.blackmagicdesign.com/"
+    strategy :page_match do
+      res, _err, _st =
+        Open3.capture3("curl -X POST -H \"Content-Type: application/json\" -d '{\"product\":" \
+                       "\"blackmagic-cloud-store\", " \
+                       "\"platform\":\"mac\"}' \"https://www.blackmagicdesign.com/api/support/latest-version\"")
+      version_info = JSON.parse(res)["mac"]
+      next if version_info.blank?
+
+      "#{version_info["major"]}.#{version_info["minor"]}.#{version_info["releaseNum"]},#{version_info["releaseId"]},#{version_info["downloadId"]}"
+    end
+  end
+
+  # Doesn't automatically update, but set to true to prevent `brew upgrade` from forcing an update
+  auto_updates true
+  container nested: "Blackmagic_Cloud_Store_#{version.csv.first.chomp(".0")}.dmg"
+
+  pkg "Install Cloud Store #{version.csv.first.chomp(".0")}.pkg"
+
+  uninstall script:  {
+              executable: "/Applications/Blackmagic Cloud Store/Uninstall Cloud Store.app/Contents/Resources/uninstall.sh",
+              sudo:       true,
+            },
+            pkgutil: [
+              "com.blackmagic-design.SharedStorageUninstaller",
+              "com.blackmagic-design.SharedStorageAssets",
+              "com.blackmagic-design.SharedStorage",
+              "com.blackmagic-design.ManifestProxyGenerator",
+            ]
+
+  zap trash: [
+    "~/Library/Preferences/com.blackmagic-design.Cloud Store Setup.plist",
+    "~/Library/Saved Application State/com.blackmagic-design.cloudstore.utility.savedState",
+  ]
+end

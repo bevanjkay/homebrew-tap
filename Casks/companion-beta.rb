f12f4e8f2e56d1b54ae06256bdb70a29e97f1a5f
Bevan Kay <email@bevankay.me>
2023-05-24 15:27:38 +1000

    companion-beta 3.0.0+5898-beta-1c76b4c0 (#165)

--- Casks/companion-beta.rb
+++ Casks/companion-beta.rb
@@ -1,9 +1,9 @@
 cask "companion-beta" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.0.0+5897-beta-816bafa7"
-  sha256 arm:   "c26f3b4e4ffa5ba57bdd81419f4955ebc324748d5b813995d34662152f2a150f",
-         intel: "77c6169c330378a168bd656ab18850733e3cd333cb6759a1f623edeeb45ca0e4"
+  version "3.0.0+5898-beta-1c76b4c0"
+  sha256 arm:   "b99f9f1f9837d685bbc47478fe95cab4ec8bd61bed2cc3869b8039e75d5640db",
+         intel: "836465a3bbf0af6cf126347546621f0cebf64cd374795c0375cdc557f737947a"
 
   url "https://s3.bitfocus.io/builds/companion/companion-mac-#{arch}-#{version}.dmg"
   name "Bitfocus Companion"

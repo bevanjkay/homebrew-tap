cbd935ccc6c1fb0a0c259488e4730b3bba7287c4
Bevan Kay <email@bevankay.me>
2023-06-13 11:24:03 +1000

    ndi-tools: rename to ndi-core-suite (#187)

--- Casks/ndi-core-suite.rb
+++ Casks/ndi-core-suite.rb
@@ -1,12 +1,12 @@
-cask "ndi-tools" do
+cask "ndi-core-suite" do
   version "5.5"
   sha256 :no_check
 
   url "https://downloads.ndi.tv/Tools/NDIToolsInstaller.pkg",
       verified: "downloads.ndi.tv/Tools/"
-  name "NDI Tools"
+  name "NDI Core Suite"
   desc "Tools & plugins for NDI"
-  homepage "https://ndi.video/tools/ndi-tools"
+  homepage "https://ndi.video/tools/ndi-core-suite/"
 
   # TODO: https://downloads.ndi.video/Tools/ndi_tools_osx_current_version.json
   livecheck do

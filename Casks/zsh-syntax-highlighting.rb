6c9c2f049b9e226311b3802abfbd52f984d75e87
Bevan Kay <email@bevankay.me>
2023-06-29 09:25:26 +1000

    zsh-syntax-highlighting 0.7.1

--- Casks/zsh-syntax-highlighting.rb
+++ Casks/zsh-syntax-highlighting.rb
@@ -7,5 +7,11 @@ cask "zsh-syntax-highlighting" do
   desc "Fish-like syntax highlighting for zsh"
   homepage "https://github.com/zsh-users/zsh-syntax-highlighting"
 
-  suite "zsh-syntax-highlighting-#{version}", target: "#{Dir.home}/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
+  suite "zsh-syntax-highlighting-#{version}", target: "#{Dir.home}/.oh-my-zsh-custom/plugins/zsh-syntax-highlighting"
+
+  zap rmdir: "#{Dir.home}/.oh-my-zsh-custom"
+
+  caveats do
+    "Add the following to your .zshrc file - ZSH_CUSTOM=$HOME/.oh-my.zsh-custom"
+  end
 end

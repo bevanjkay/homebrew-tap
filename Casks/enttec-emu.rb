1ff1f614f5cb4029a650470f4627536bba994c98
Bevan Kay <email@bevankay.me>
2023-07-17 16:33:16 +1000

    enttec-emu 23.05.31.4

---
+++ Casks/enttec-emu.rb
@@ -0,0 +1,25 @@
+cask "enttec-emu" do
+  version "23.05.31.4"
+  sha256 "8f6f96422f9b0a4f46fb6a33649fea9bfcee9ca83a957e0f6ec379eaa0446385"
+
+  url "https://s3-us-west-2.amazonaws.com/enttec-software-builds/emu/EMU-#{version}.pkg",
+      verified: "s3-us-west-2.amazonaws.com/enttec-software-builds/emu/"
+  name "ENTTEC Emu"
+  desc "Setup and manage ENTTEC DMX devices"
+  homepage "https://www.enttec.com.au/product/dmx-lighting-control-software/emu-sound-to-light-controller/"
+
+  livecheck do
+    url "https://s3-us-west-2.amazonaws.com/enttec-software-builds/emu/emu-versions.json"
+    regex(/EMU[._-]v?(\d+(?:\.\d+)+)\.pkg","Stages":\["Beta","Production/i)
+  end
+
+  pkg "EMU-#{version}.pkg"
+
+  uninstall pkgutil: [
+    "com.enttec.emu.au",
+    "com.enttec.emu.vst3",
+    "com.enttec.emu",
+  ]
+
+  zap trash: "~/Library/Preferences/com.enttec.emu.plist"
+end

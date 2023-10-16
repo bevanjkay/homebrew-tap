5030a3981ff121df4371a0a513191c6e540ba68a
Bevan Kay <email@bevankay.me>
2023-10-16 14:06:52 +1100

    mixing-station-beta 2.0.0

---
+++ Casks/mixing-station-beta.rb
@@ -0,0 +1,28 @@
+cask "mixing-station-beta" do
+  version "2.0.0"
+  sha256 :no_check
+
+  url "https://mixingstation.app/backend/api/web/download/attachment/mixing-station-pc/beta/OSX"
+  name "Mixing Station"
+  desc "Audio Mixer Controller"
+  homepage "https://mixingstation.app/"
+
+  livecheck do
+    url "https://mixingstation.app/backend/api/web/changelogs/milestones"
+    strategy :json do |json|
+      json["data"].select { |item| item["variant"] == "PC" }
+                  .map { |item| item["beta"]["name"] }
+    end
+  end
+
+  # Doesn't automatically update, but set to true to prevent `brew upgrade` from forcing an update
+  auto_updates true
+  conflicts_with cask: "bevanjkay/tap/mixing-station"
+
+  app "mixing-station-pc.app", target: "Mixing Station.app"
+
+  zap trash: [
+    "~/Library/Saved Application State/org.devcore.mixingstation.pc.savedState",
+    "~/MixingStation",
+  ]
+end

c17a7e552178418f2cf8b5f12583ca5a6710b98e
Bevan Kay <email@bevankay.me>
2023-11-02 09:12:45 +1100

    allen-and-heath-midi-control 2.01,2023,02

--- Casks/allen-and-heath-midi-control.rb
+++ Casks/allen-and-heath-midi-control.rb
@@ -1,15 +1,18 @@
 cask "allen-and-heath-midi-control" do
-  version "2.01"
+  version "2.01,2023,02"
   sha256 "4479564b5cac657ce166704083702a04738b7e152fc89ee79413ab6fa7299bbf"
 
-  url "https://www.allen-heath.com/media/Allen-and-Heath-MIDI-Control-#{version}-Mac.zip"
+  url "https://www.allen-heath.com/content/uploads/#{version.csv.second}/#{version.csv.third}/Allen-and-Heath-MIDI-Control-#{version.csv.first}-Mac.zip"
   name "Allen & Heath Midi Control"
   desc "Midi control software for Allen & Heath audio consoles"
   homepage "https://www.allen-heath.com/midi-control/"
 
   livecheck do
-    url :homepage
-    regex(/A&H\s*MIDI\s*Control\s*v?(\d+(?:\.\d+)+)[< "]/i)
+    url "https://www.allen-heath.com/hardware/controllers/midi-control/resources/"
+    regex(%r{href=.*?/([^/]+)/([^/]+)/Allen-and-Heath-MIDI-Control[._-]v?(\d+(?:\.\d+)+)(?:-Mac)?\.zip}i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| "#{match.third},#{match.first},#{match.second}" }
+    end
   end
 
   container nested: "Allen and Heath MIDI Control #{version}.dmg"

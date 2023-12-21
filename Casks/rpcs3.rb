9048013df9a059b0f00225aa7e5e9bfa5a45da83
Bevan Kay <email@bevankay.me>
2023-12-21 14:28:30 +1100

    rpcs3 0.0.29-15834-b17b6e07

--- Casks/rpcs3.rb
+++ Casks/rpcs3.rb
@@ -1,22 +1,23 @@
 cask "rpcs3" do
-  version "0.0.29-15786-fbeaa1c4,fbeaa1c487437fe90a565803f4d45738952c1f08"
-  sha256 "056362c5e026dacbe1b3d126c17ffdbe9609977fbceec86ef8189d52a5839b1f"
+  version "0.0.29-15834-b17b6e07,b17b6e07eabdc69b040a9f84e4252d2ac9090f63"
+  sha256 "ba657f48add9ac88b46146c24f2ace72163597d570e1807b57eb1d664c8f5ed5"
 
-  url "https://github.com/RPCS3/rpcs3-binaries-mac/releases/download/build-#{version.csv.second}/rpcs3-v#{version.csv.first}_macos.dmg",
+  url "https://github.com/RPCS3/rpcs3-binaries-mac/releases/download/build-#{version.csv.second}/rpcs3-v#{version.csv.first}_macos.7z",
       verified: "github.com/RPCS3/rpcs3-binaries-mac/"
   name "RPCS3"
   desc "PS3 emulator"
   homepage "https://rpcs3.net/"
 
   livecheck do
-    url "https://rpcs3.net/compatibility?b"
-    regex(%r{href=.*?([^-]+)/rpcs3[._-]v?((?:\d+(?:[.-]\d+)+)[._-](?:[a-f]|[0-9])+)[._-]macos\.dmg}i)
+    url "https://rpcs3.net/download"
+    regex(%r{href=.*?([^-]+)/rpcs3[._-]v?((?:\d+(?:[.-]\d+)+)[._-](?:[a-f]|[0-9])+)[._-]macos\.7z}i)
     strategy :page_match do |page, regex|
       page.scan(regex).map { |match| "#{match.second},#{match.first}" }
     end
   end
 
   depends_on macos: ">= :monterey"
+  container type: :seven_zip
 
   app "RPCS3.app"
 

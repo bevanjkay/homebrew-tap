053567775e794c3a415103b3b473673dcd2b29e0
Bevan Kay <email@bevankay.me>
2024-01-23 23:03:44 +1100

    adobe-fonts-revealer: fix style

--- Casks/adobe-fonts-revealer.rb
+++ Casks/adobe-fonts-revealer.rb
@@ -3,7 +3,7 @@ cask "adobe-fonts-revealer" do
   sha256 :no_check
 
   url "https://github.com/Kalaschnik/adobe-fonts-revealer.git",
-      branch:   "main"
+      branch: "main"
   name "Adobe Fonts Revealer"
   desc "Turn Adobe Fonts into local fonts"
   homepage "https://github.com/Kalaschnik/adobe-fonts-revealer"

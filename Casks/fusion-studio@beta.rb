e0eb68bb9b53b336adb98e13e330b5ca67a73d8c
Bevan Kay <email@bevankay.me>
2024-05-08 22:07:43 +1000

    fusion-studio@beta 19.0.0

---
+++ Casks/fusion-studio@beta.rb
@@ -0,0 +1,76 @@
+cask "fusion-studio@beta" do
+  require "net/http"
+
+  version "19.0.0,142b53d9c9d6447aaaca646ebe96e5cb,ec16437199f04244a815453605d2257d,b"
+  sha256 "f7988fc85645c14d9904a9dbe0c3a450a8531330773e9a1446a351b9eeb6a497"
+
+  url do
+    params = {
+      "platform"     => "Mac OS X",
+      "product"      => {
+        "name" => "Fusion Studio",
+      },
+      "policy"       => "true",
+      "downloadOnly" => "true",
+      "country"      => "au",
+    }.to_json
+
+    uri = URI("https://www.blackmagicdesign.com/api/register/au/download/#{version.csv.third}")
+    resp = Net::HTTP.post(uri, params, { "Content-Type" => "application/json" })
+
+    resp.body
+  end
+  name "Fusion Studio"
+  desc "Visual effects software"
+  homepage "https://www.blackmagicdesign.com/au/products/fusion/"
+
+  livecheck do
+    url "https://www.blackmagicdesign.com/api/support/us/downloads.json"
+    strategy :json do |json|
+      matched = json["downloads"].select do |download|
+        next false if download["urls"]["Mac OS X"].blank?
+
+        download["urls"]["Mac OS X"].first["product"] == "fusion-studio"
+      end
+      matched.map do |download|
+        beta = /beta/i.match?(download["name"])
+        v = download["urls"]["Mac OS X"].first
+        "#{v["major"]}.#{v["minor"]}.#{v["releaseNum"]},#{v["releaseId"]},#{v["downloadId"]},#{beta ? "b" : ""}"
+      end
+    end
+  end
+
+  auto_updates true
+  conflicts_with cask: "fusion-studio"
+
+  pkg "Install Fusion Studio v#{version.csv.first.chomp(".0")}.pkg"
+  pkg "Install Fusion Render Node v#{version.csv.first.chomp(".0")}.pkg"
+
+  # Split uninstall stanza to allow multiple scripts
+  uninstall script: {
+    executable: "/Applications/Blackmagic Fusion #{version.major}/Uninstall Fusion.app/Contents/Resources/uninstall.sh",
+    sudo:       true,
+  }
+  # Quit render node to enable uninstall
+  uninstall quit: "com.blackmagic-design.fusionrendernode"
+  uninstall script: {
+    executable: "/Applications/Blackmagic Fusion #{version.major} Render Node/Uninstall Fusion Render Node.app/Contents/Resources/uninstall.sh",
+    sudo:       true,
+  }
+  uninstall launchctl: "com.blackmagicdesign.fusion.server",
+            pkgutil:   [
+              "com.blackmagic-design.Fusion#{version.major}RenderNode",
+              "com.blackmagic-design.Fusion#{version.major}RenderNodeAssets",
+              "com.blackmagic-design.Fusion#{version.major}RenderNodeUninstaller",
+              "com.blackmagic-design.Fusion#{version.major}Studio",
+              "com.blackmagic-design.Fusion#{version.major}StudioAssets",
+              "com.blackmagic-design.Fusion#{version.major}StudioUninstaller",
+            ]
+
+  zap trash: [
+    "/Library/LaunchDaemons/com.blackmagicdesign.fusion.server.plist",
+    "~/Library/Preferences/com.blackmagic-design.fusion.plist",
+    "~/Library/Preferences/com.blackmagic-design.fusionrendernode.plist",
+    "~/Library/Saved Application State/com.blackmagic-design.fusion.savedState",
+  ]
+end

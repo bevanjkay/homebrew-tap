e5e399e299e683262500d83a79964c99e309c038
Bevan Kay <email@bevankay.me>
2024-05-28 20:13:59 +1000

    bible-import: test re-run workflow

--- Casks/bible-import.rb
+++ Casks/bible-import.rb
@@ -1,5 +1,5 @@
 cask "bible-import" do
-  version "2024-05-04"
+  version "2024-05-05"
   sha256 "58325e5cbc499a80a05d4a700cd44ac8bfeae4001f1941917fe1a30e9b9e98a8"
 
   url "https://github.com/martijnlentink/propresenter-custom-bibles/releases/download/#{version}/bible_import.OSX.tar.zip"

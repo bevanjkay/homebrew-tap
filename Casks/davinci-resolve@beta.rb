7f9861ccbd45f257c4c9379de924e368a65f3428
Bevan Kay <email@bevankay.me>
2024-08-22 21:51:58 +1000

    davinci-resolve@beta: fix `pkg` stanza

--- Casks/davinci-resolve@beta.rb
+++ Casks/davinci-resolve@beta.rb
@@ -66,7 +66,7 @@ cask "davinci-resolve@beta" do
   auto_updates true
   conflicts_with cask: "davinci-resolve"
 
-  pkg "Install Resolve #{version.csv.first}#{version.csv.fourth}.pkg"
+  pkg "Install Resolve #{version.csv.first.chomp(".0")}#{version.csv.fourth}.pkg"
 
   uninstall script:  {
               executable: "/Applications/DaVinci Resolve/Uninstall Resolve.app/Contents/Resources/uninstall.sh",

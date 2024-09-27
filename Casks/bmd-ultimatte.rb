0e2e2b2ab6a18e452b21056a7edb273e65f71f90
Bevan Kay <email@bevankay.me>
2024-09-27 16:56:13 +1000

    bmd-ultimatte: remove container stanza

--- Casks/bmd-ultimatte.rb
+++ Casks/bmd-ultimatte.rb
@@ -34,7 +34,6 @@ cask "bmd-ultimatte" do
 
   # Doesn't automatically update, but set to true to prevent `brew upgrade` from forcing an update
   auto_updates true
-  container nested: "Blackmagic_Ultimatte_#{version.csv.first.chomp(".0")}.dmg"
 
   pkg "Install Ultimatte #{version.csv.first.chomp(".0")}.pkg"
 

9ad1db7fd2b4dc9febf39aebfaf03f81696c8f6f
Bevan Kay <email@bevankay.me>
2025-01-07 23:42:26 +1100

    cmd/disable-deprecated-packages: fix typechecking

--- cmd/disable-deprecated-packages.rb
+++ cmd/disable-deprecated-packages.rb
@@ -1,7 +1,12 @@
 # typed: strict
 # frozen_string_literal: true
 
+require "abstract_command"
+require "formula"
+require "cask/cask"
+require "open3"
 require "date"
+
 module Homebrew
   module Cmd
     class DisableDeprecatedPackages < AbstractCommand
@@ -9,22 +14,25 @@ module Homebrew
         usage_banner <<~EOS
           `disable-deprecated-packages` <tap>
 
-          Disables packages deprecated for more than 12 months.
+          Disables deprecated packages in the specified tap.
         EOS
 
         named_args :tap, min: 1, max: 1
       end
 
+      sig { override.void }
       def run
         ENV["HOMEBREW_EVAL_ALL"] = "1"
         tap_string = args.named.first
-        @target_tap = Tap.fetch(tap_string)
+        @target_tap = T.let(Tap.fetch(T.must(tap_string)), T.nilable(Tap))
+
+        odie "Invalid tap: #{tap_string}" if @target_tap.nil?
 
         packages_to_disable = find_deprecated(packages: Formula.all + Cask::Cask.all)
 
         if packages_to_disable.any?
           branch_name = "disable-packages-#{Date.today.strftime("%Y-%m-%d")}"
-          git "-C", @target_tap.path.to_s, "checkout", "-b", branch_name unless @dry_run
+          git "-C", @target_tap.path.to_s, "checkout", "-b", branch_name
         end
 
         puts "Disabling deprecated packages..."
@@ -62,11 +70,13 @@ module Homebrew
         end
       end
 
+      sig { params(args: ::String).void }
       def git(*args)
         system "git", *args
         exit $CHILD_STATUS.exitstatus unless $CHILD_STATUS.success?
       end
 
+      sig { params(packages: T::Array[T.any(Formula, Cask::Cask)]).returns(T::Array[T.any(Formula, Cask::Cask)]) }
       def find_deprecated(packages: [])
         puts "Finding deprecated packages..."
         twelve_months_ago = Date.today << 12 # Subtracts 12 months from the current date
@@ -77,12 +87,14 @@ module Homebrew
           next false if package.disabled?
           next false if package.deprecation_date.nil?
 
-          package.deprecation_date < twelve_months_ago
+          package.deprecation_date <= twelve_months_ago
         end
       end
 
+      sig { params(package: T.any(Formula, Cask::Cask)).returns(String) }
       def sourcefile_path(package)
-        package.is_a?(Formula) ? package.path : package.sourcefile_path
+        package.path.to_s unless package.is_a?(Cask::Cask)
+        package.sourcefile_path.to_s if package.is_a?(Cask::Cask)
       end
     end
   end

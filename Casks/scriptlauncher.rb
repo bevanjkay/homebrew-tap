70c13ce7b5359b847ac04112292acccffa0bacdb
Bevan Kay <email@bevankay.me>
2025-12-01 16:38:30 +0800

    scriptlauncher: update intel checksum

--- Casks/scriptlauncher.rb
+++ Casks/scriptlauncher.rb
@@ -2,7 +2,8 @@ cask "scriptlauncher" do
   arch arm: "silicon", intel: "intel"
 
   version "1.9.0"
-  sha256 "66e426d60c6965dd27e8625b703b0e4b5bece3e3b7e82e5570a57fcc51739c45"
+  sha256 arm:   "66e426d60c6965dd27e8625b703b0e4b5bece3e3b7e82e5570a57fcc51739c45",
+         intel: "ede696c4d9be07e6616a952f2a7006fe859c80bde4216b31b8082e1211f9a107"
 
   url "https://github.com/josephdadams/ScriptLauncher/releases/download/v#{version}/scriptlauncher-mac-#{arch}-#{version}.dmg"
   name "ScriptLauncher"

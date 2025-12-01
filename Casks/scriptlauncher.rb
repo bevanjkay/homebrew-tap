01f08a09a8d472254f756460f28d70319d712280
Bevan Kay <email@bevankay.me>
2025-12-01 19:03:06 +1100

    scriptlauncher 1.9.0 (new cask)

---
+++ Casks/scriptlauncher.rb
@@ -0,0 +1,21 @@
+cask "scriptlauncher" do
+  arch arm: "silicon", intel: "intel"
+
+  version "1.9.0"
+  sha256 "66e426d60c6965dd27e8625b703b0e4b5bece3e3b7e82e5570a57fcc51739c45"
+
+  url "https://github.com/josephdadams/ScriptLauncher/releases/download/v#{version}/scriptlauncher-mac-#{arch}-#{version}.dmg"
+  name "ScriptLauncher"
+  desc "Run scripts on your machine"
+  homepage "https://github.com/josephdadams/ScriptLauncher"
+
+  depends_on macos: ">= :big_sur"
+
+  app "scriptlauncher.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.josephadams.scriptlauncher.sfl*",
+    "~/Library/Application Support/scriptlauncher",
+    "~/Library/Preferences/com.josephadams.scriptlauncher.plist",
+  ]
+end

6dd96a4ec4b0b38f6e0758fe4933614b2898d013
Bevan Kay <email@bevankay.me>
2026-01-09 13:15:04 +1100

    mas-static: update `depends_on`

--- Casks/mas-static.rb
+++ Casks/mas-static.rb
@@ -1,9 +1,16 @@
 cask "mas-static" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "5.0.2"
-  sha256 arm:   "b1deee20815c8cad9ea6da42136f7fc30010d94f33b63f85ae835b160eb317d5",
-         intel: "903892ad8238aeb849e45f1ce452b09dc0467f77fe545c719f592f77356c187c"
+  on_monterey :or_older do
+    version "4.1.2"
+    sha256 arm:   "4e31579678c5878ddf5d203f7b55ddb8ab9f2edaefb07aae310fac65cf7ea715",
+           intel: "89dd223c3920501e4509a288cda0955e1008b710e78cb33dbd6b25186fa94b03"
+  end
+  on_ventura :or_newer do
+    version "5.0.2"
+    sha256 arm:   "b1deee20815c8cad9ea6da42136f7fc30010d94f33b63f85ae835b160eb317d5",
+           intel: "903892ad8238aeb849e45f1ce452b09dc0467f77fe545c719f592f77356c187c"
+  end
 
   url "https://github.com/mas-cli/mas/releases/download/v#{version}/mas-#{version}-#{arch}.pkg"
   name "mas-static"

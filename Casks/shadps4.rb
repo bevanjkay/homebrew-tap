65162444af101cd189457037a7d0f9ccb8517de7
bevanjkay <email@bevankay.me>
2026-03-08 23:28:45 +1100

    shadps4 2026-03-04

--- Casks/shadps4.rb
+++ Casks/shadps4.rb
@@ -1,6 +1,6 @@
 cask "shadps4" do
-  version "2026-03-03,9bccf1efc73f336cb7880d91377d467ff2219831"
-  sha256 "2af79a477f44a92375953c19006158bf19f3bdc82a77285abf7a908ad4549998"
+  version "2026-03-04,034d58e17f1667863d32cf38cec172a4b19b7abe"
+  sha256 "3359aa217b6f078ba8756e921de7ebe11ebf8ace0f659ccd3c4fd2928944007d"
 
   url "https://github.com/shadps4-emu/shadps4-qtlauncher/releases/download/shadPS4QtLauncher-#{version.tr(",", "-")}/shadPS4QtLauncher-macos-qt-#{version.csv.first}-#{version.csv.second&.slice(0, 7)}.zip",
       verified: "github.com/shadps4-emu/shadps4-qtlauncher/"

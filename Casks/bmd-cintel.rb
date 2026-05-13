91f3ab66dccb57f56b578fce0e3bdbe4b4d153e4
Bevan Kay <email@bevankay.me>
2026-05-13 22:45:53 +1000

    bmd-cintel: style fixes

--- Casks/bmd-cintel.rb
+++ Casks/bmd-cintel.rb
@@ -34,6 +34,7 @@ cask "bmd-cintel" do
 
   # Doesn't automatically update, but set to true to prevent `brew upgrade` from forcing an update
   auto_updates true
+  depends_on :macos
 
   pkg "Install Cintel #{version.csv.first.chomp(".0")}.pkg"
 

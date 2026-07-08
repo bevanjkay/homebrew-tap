68507107d7c31206382673d9571dbc5744326aee
Bevan Kay <email@bevankay.me>
2026-07-08 15:43:46 +1000

    whatsapp: test zap CI

---
+++ Casks/whatsapp.rb
@@ -0,0 +1,32 @@
+cask "whatsapp" do
+  version "26.26.19"
+  sha256 "ac44113b973eca98f8fd54072d53e132144d8e54e5fc1c9699379f6c2c7fa7b7"
+
+  url "https://web.whatsapp.com/desktop/mac_native/release/?version=2.#{version}&extension=zip&configuration=Release&branch=master&is_buck=true"
+  name "WhatsApp"
+  desc "Native desktop client for WhatsApp"
+  homepage "https://www.whatsapp.com/"
+
+  livecheck do
+    url "https://web.whatsapp.com/desktop/mac_native/updates/?branch=master&configuration=Release"
+    strategy :sparkle, &:short_version
+  end
+
+  auto_updates true
+  conflicts_with cask: "whatsapp@beta"
+  depends_on macos: :monterey
+
+  # test
+  app "WhatsApp.app"
+
+  uninstall quit: "net.whatsapp.WhatsApp"
+
+  zap trash: [
+    "~/Library/Application Scripts/net.whatsapp.WhatsApp*",
+    "~/Library/Caches/net.whatsapp.WhatsApp",
+    "~/Library/Containers/net.whatsapp.WhatsApp*",
+    "~/Library/Group Containers/group.com.facebook.family",
+    "~/Library/Group Containers/group.net.whatsapp*",
+    "~/Library/Saved Application State/net.whatsapp.WhatsApp.savedState",
+  ]
+end
